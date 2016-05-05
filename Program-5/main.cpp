e8b9be21ef6fce0e6aa14842c2a89e157dd0c21b
bqueze <bethelquezecrizikz@yahoo.com>
2016-05-05 13:43:23 -0500

    Create main.cpp

---
+++ Program-5/main.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include "Menu_v2.hpp"
+#include "blog.hpp"
+using namespace std;
+
+int main()
+{
+	Blog blog;
+	blog.run();
+
+	return 0;
+}
